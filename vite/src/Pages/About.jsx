9a410ce27178664685a62d991db8012fc055fd50
Ibrahim Abboud <coraynova@hotmail.com>
2023-03-08 22:43:27 +0200

    added my resume to the about page

--- vite/src/Pages/About.jsx
+++ vite/src/Pages/About.jsx
@@ -1,9 +1,45 @@
 import styles from './About.module.css'
+import power from '../Assets/power.svg'
+import resume from '../Assets/resume.png'
+import { useState } from 'react'
+import Wrapper from '../Components/UI/Wrapper'
+import View from '../Assets/View'
+import Download from '../Assets/Download'
 
 export default function About(){
+    const [showModal, setShowModal] = useState(false)
+
     return (
-        <div className="container">
-           about
-        </div>
+        <Wrapper>
+            <div className={styles.container}>
+                <div className={styles.buttons}>
+                    <button className={styles.view} onClick={() => setShowModal(true)}>
+                        <span>View</span>
+                        <View/>
+                    </button>
+                    <a href={resume} download>
+                        <span>Download Resume</span>
+                        <Download/>
+                    </a>
+                </div>
+                <div className={styles.computer}>
+                    <div className={styles.screenBorder}>
+                        <img src={resume}/>
+                        <div className={styles.screen}>
+                            <button className={styles.power}>
+                                <img src={power} onClick={() => setShowModal(true)}/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className={styles.stand}></div>
+                    <div className={styles.flat}></div>
+                </div>
+            </div>
+
+            {showModal && <div className={styles.backdrop} onClick={() => setShowModal(false)}></div>}
+            {showModal && 
+                <img src={resume} className={styles.modal}/>
+            }
+        </Wrapper>
     )
 }
