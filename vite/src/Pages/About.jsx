b521918499c78d8331992ae19b2f3478be3a196a
Ibrahim Abboud <coraynova@hotmail.com>
2023-03-28 23:30:23 +0300

    added technology section, removed monitor

--- vite/src/Pages/About.jsx
+++ vite/src/Pages/About.jsx
@@ -1,19 +1,19 @@
 import styles from './About.module.css'
-import power from '../Assets/power.svg'
 import resume from '../Assets/ibrahimabboudresume.png'
 import pdf from '../Assets/ibrahimabboud.pdf'
 import { useState } from 'react'
-import Wrapper from '../Components/UI/Wrapper'
 import DownloadIcon from '@mui/icons-material/Download';
 import Button from '@mui/material/Button';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import Stack from '@mui/material/Stack';
 
+import technologies from './Technologies'
+
 export default function About(){
     const [showModal, setShowModal] = useState(false)
 
     return (
-        <Wrapper>
+        <div className={styles.about}>
             <div className={styles.aboutme}>
                 <div className={styles.content}>
                     <h2>About Ibrahim Abboud</h2>
@@ -28,7 +28,21 @@ export default function About(){
                     </p>
                 </div>
             </div>
+
+            <div className={styles.tech}>
+                <h1 className={styles.title}>Most used technologies</h1>
+
+                <div className={styles.langs}>
+                    {technologies.map(lang => (
+                    <div className={styles.lang} key={lang.title}>
+                        {lang.logo}
+                        <span>{lang.title}</span>
+                    </div>
+                    ))}
+                </div>
+            </div>
             <div className={styles.container}>
+                <h1 style={{color: "white"}}>Resume</h1>
                 <div className={styles.buttons}>
                     <Stack direction="row" spacing={2}>
                         <Button variant="outlined" endIcon={<VisibilityIcon />} onClick={() => setShowModal(true)}>
@@ -41,24 +55,12 @@ export default function About(){
                         </a>
                     </Stack>
                 </div>
-                <div className={styles.computer}>
-                    <div className={styles.screenBorder}>
-                        <img src={resume}/>
-                        <div className={styles.screen}>
-                            <button className={styles.power}>
-                                <img src={power} onClick={() => setShowModal(true)}/>
-                            </button>
-                        </div>
-                    </div>
-                    <div className={styles.stand}></div>
-                    <div className={styles.flat}></div>
-                </div>
             </div>
 
             {showModal && <div className={styles.backdrop} onClick={() => setShowModal(false)}></div>}
             {showModal && 
                 <img src={resume} className={styles.modal}/>
             }
-        </Wrapper>
+        </div>
     )
 }
---
+++ vite/src/Pages/Technologies.jsx
@@ -0,0 +1,71 @@
+import Css from '../Assets/langs/Css'
+import Flask from '../Assets/langs/Flask'
+import Html from '../Assets/langs/Html'
+import Javascript from '../Assets/langs/Javascript'
+import Reacts from '../Assets/langs/Reacts'
+import Sass from '../Assets/langs/Sass'
+import Git from '../Assets/langs/Git'
+import Bootstrap from '../Assets/langs/Bootstrap'
+import Vite from '../Assets/langs/Vite'
+import NodePM from '../Assets/langs/NodePM'
+import Photoshop from '../Assets/langs/Photoshop'
+import Django from '../Assets/langs/Django'
+import Mongo from '../Assets/langs/Mongo'
+
+const technologies = [
+    {
+        title: "Javascript",
+        logo: <Javascript/>
+    },
+    {
+        title: "React",
+        logo: <Reacts/>
+    },
+    {
+        title: "Flask",
+        logo: <Flask/>
+    },
+    {
+        title: "Vite",
+        logo: <Vite/>
+    },
+    {
+        title: "HTML5",
+        logo: <Html/>
+    },
+    {
+        title: "CSS3",
+        logo: <Css/>
+    },
+    {
+        title: "SASS",
+        logo: <Sass/>
+    },
+    {
+        title: "Git",
+        logo: <Git/>
+    },
+    {
+        title: "Django",
+        logo: <Django/>
+    },
+    {
+        title: "Bootstrap",
+        logo: <Bootstrap/>
+    },
+    {
+        title: "NPM",
+        logo: <NodePM/>
+    },
+    {
+        title: "Mongo",
+        logo: <Mongo/>
+    },
+    {
+        title: "Photoshop",
+        logo: <Photoshop/>
+    },
+]
+
+
+export default technologies
