a87eff813d4d7165c7040c34cc0ce5a2e5cd36a4
Ibrahim abboud <coraynova@hotmail.com>
2023-09-14 15:25:46 +0300

    added 404 page

---
+++ vite/src/Components/UI/Error404.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import styles from './Error404.module.css'
+
+export default function Error404() {
+  const [trapped, setIsTrapped] = useState(false)
+  const [safe, setIsSafe] = useState(false)
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.shape}>
+        <div className={styles.spooky}>
+          <div className={`${styles.eyes} ${safe ? styles.sadeyes : ''}`}>
+            <span className={trapped ? styles.trapped : ''}></span>
+            <span className={trapped ? styles.trapped : ''}></span>
+          </div>
+
+          <div className={`${styles.mouth} ${trapped && styles.happy} ${safe && styles.sad}`}></div>
+        </div>
+      </div>
+      <div className={styles.error}>
+        <div className={styles.title}>404</div>
+        <div className={styles.subtitle}>Hey, don't be spooked</div>
+        <div className={styles.message}>The page you are looking for is on the righ.. d..down there on the left.</div>
+        <div className={styles.buttons}>
+          <button onMouseOver={() => setIsTrapped(true)} onMouseOut={() => setIsTrapped(false)}>Go Home</button>
+          <button onMouseOver={() => setIsSafe(true)} onMouseOut={() => setIsSafe(false)}>Go Home</button>
+        </div>
+      </div>
+    </div>
+  )
+}
